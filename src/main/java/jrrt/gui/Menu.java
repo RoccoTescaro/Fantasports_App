7f1934252b07863e74f6bf3a46ce0bb6f207288e
RoccoTescaro <rocco.tescaro@stud.unifi.it>
2024-02-17 00:38:22 +0100

    Created a padding for menu gui

--- src/main/java/jrrt/gui/Menu.java
+++ src/main/java/jrrt/gui/Menu.java
@@ -1,26 +1,32 @@
 package jrrt.gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Menu {
 
     private final JFrame frame;
-    private final JPanel contentPane; // Use a content pane for layout and background
+    private final JPanel content_panel; // Use a content pane for layout and background
 
     public Menu() 
     {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        contentPane = new JPanel(); // Create the content pane
-        contentPane.setLayout(new BorderLayout()); // Use BorderLayout for flexibility
-        frame.setContentPane(contentPane); // Set the content pane
+        //
+        content_panel = new JPanel();
 
-        // Set the background color of the content pane
-        contentPane.setBackground(new Color(40, 42, 54));
+        content_panel.setLayout(new BorderLayout()); // Use BorderLayout for flexibility
 
-        // Set frame icon (adjust path if needed)
+        Border padding = BorderFactory.createEmptyBorder(16, 48, 16, 48);
+        content_panel.setBorder(padding);
+        
+        content_panel.setBackground(new Color(40, 42, 54));
+        
+        frame.setContentPane(content_panel);
+        //    
+    
         ImageIcon icon = new ImageIcon(getClass().getResource("/images/icon.png"));
         frame.setIconImage(icon.getImage());
         
@@ -39,9 +45,9 @@ public class Menu {
         JLabel label = new JLabel(text);
         label.setForeground(new Color(248, 248, 242));
         label.setFont(new Font("Arial", Font.PLAIN, 20));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setHorizontalAlignment(SwingConstants.LEFT);
         label.setVerticalAlignment(SwingConstants.CENTER);
-        contentPane.add(label, BorderLayout.CENTER);
+        content_panel.add(label, BorderLayout.PAGE_START);
         refresh();
     }
 
